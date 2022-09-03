7af486b83fece39b6601cce5c206da5514f8fd0f
Filin <filin@ymail.com>
2022-09-03 22:58:08 +0300

    tf patch

---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {  
+  type        = string  
+  default     = "eu-west-1"  
+  description = "Name of the s3 bucket to be created."
+}
+
+variable "bucket_prefix" {  
+  type        = string  
+  description = "Name of the s3 bucket to be created."
+} 
